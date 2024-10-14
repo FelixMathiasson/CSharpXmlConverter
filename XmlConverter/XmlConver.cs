528301f39175d20c8813b85f92d29e4b01229280
Felix <felix.mathiasson99@gmail.com>
2024-10-14 18:05:20 +0200

    started on XML converter class

--- XmlConverter/XmlConver.cs
+++ XmlConverter/XmlConver.cs
@@ -4,11 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
+using XmlConverter.Models;
 
 namespace XmlConverter
 {
     public class XmlConver
     {
-        //public XElement
+        public XElement MakeXML(List<Person> people)
+        {
+            // XElement basically builds the data gatherered by the parser as an XML file
+            var resultXML = new XElement("people");
+
+            foreach (var person in people)
+            {
+                //always included
+                var firstName = new XElement("firstname", person.FirstName);
+                var lastName = new XElement("lastname", person.LastName);
+
+                var element = new XElement("person", firstName, lastName);
+            }
+        }
     }
 }
