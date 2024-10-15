3c67c6bfebf0a39a1eb12f465c998d584674e010
Felix <felix.mathiasson99@gmail.com>
2024-10-15 13:48:17 +0200

    moved creation of objects in program.cs to outside the loop to avoid new creation of objects each iteration

--- XmlConverter/Program.cs
+++ XmlConverter/Program.cs
@@ -7,18 +7,18 @@ class Program
     static void Main(string[] args)
     {
         bool running = true;
-        while(running)
+        TextRead reader = new TextRead();
+        XmlConvert xmlConvert = new XmlConvert();
+
+        while (running)
         { 
             Console.WriteLine("Enter name of file to parse to XML: ");
 
             string baseDirectory = AppContext.BaseDirectory;
-            string path = Path.Combine(baseDirectory, "..", "..", "..", "Data", Console.ReadLine()); // combine the base directory with relative path to find the  data file
+            string path = Path.Combine(baseDirectory, "..", "..", "..", "Data", Console.ReadLine()); // combine the base directory with relative path to find the data file
 
             if (File.Exists(path))
             {
-                TextRead reader = new TextRead();
-                XmlConvert xmlConvert = new XmlConvert();
-
                 var xmlCode = xmlConvert.MakeXML(reader.TextParseFile(path));
 
                 Console.WriteLine(xmlCode);
